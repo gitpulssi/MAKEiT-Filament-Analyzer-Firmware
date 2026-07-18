5bc19339ec5aba77bb6d34f17c07da3310420fa9
gitpulssi <152341354+gitpulssi@users.noreply.github.com>
2026-07-18 16:17:50 -0700

    Include serial helpers in M877 command

--- Marlin/src/gcode/feature/M877.cpp
+++ Marlin/src/gcode/feature/M877.cpp
@@ -18,6 +18,7 @@
 #if ENABLED(MAKEIT_FILAMENT_ANALYZER_PHASE0)
 
 #include "../gcode.h"
+#include "../../core/serial.h"
 #include "../../feature/makeit_fa_transaction.h"
 
 void GcodeSuite::M877() {
@@ -29,20 +30,20 @@ void GcodeSuite::M877() {
   const uint32_t point_id = parser.value_ulong();
 
   MakeItFAPointParams p;
-  p.total_mm                    = parser.seenval('L') ? parser.value_float() : 100.0f;
-  p.feed_mm_min                 = parser.seenval('F') ? parser.value_float() : 100.0f;
-  p.segment_mm                  = parser.seenval('S') ? parser.value_float() : 0.35f;
-  p.max_inflight                = parser.seenval('B') ? (uint8_t)parser.value_int() : 2;
-  p.report_ms                   = parser.seenval('I') ? (uint16_t)parser.value_int() : 250;
-  p.encoder_events_per_mm       = parser.seenval('C') ? parser.value_float() : 0.685f;
-  p.pass_efficiency_pct         = parser.seenval('P') ? parser.value_float() : 95.0f;
-  p.temp_tolerance              = parser.seenval('D') ? parser.value_float() : 3.0f;
-  p.auto_stop_enabled           = parser.seenval('A') ? parser.value_bool() : true;
-  p.monitor_window_mm           = parser.seenval('W') ? parser.value_float() : 20.0f;
-  p.monitor_efficiency_pct      = parser.seenval('R') ? parser.value_float() : 85.0f;
-  p.monitor_confirm_windows     = parser.seenval('K') ? (uint8_t)parser.value_int() : 2;
-  p.pulse_gap_factor            = parser.seenval('G') ? parser.value_float() : 4.0f;
-  p.pulse_gap_min_ms            = parser.seenval('H') ? (uint16_t)parser.value_int() : 500;
+  p.total_mm                     = parser.seenval('L') ? parser.value_float() : 100.0f;
+  p.feed_mm_min                  = parser.seenval('F') ? parser.value_float() : 100.0f;
+  p.segment_mm                   = parser.seenval('S') ? parser.value_float() : 0.35f;
+  p.max_inflight                 = parser.seenval('B') ? (uint8_t)parser.value_int() : 2;
+  p.report_ms                    = parser.seenval('I') ? (uint16_t)parser.value_int() : 250;
+  p.encoder_events_per_mm        = parser.seenval('C') ? parser.value_float() : 0.685f;
+  p.pass_efficiency_pct          = parser.seenval('P') ? parser.value_float() : 95.0f;
+  p.temp_tolerance               = parser.seenval('D') ? parser.value_float() : 3.0f;
+  p.auto_stop_enabled            = parser.seenval('A') ? parser.value_bool() : true;
+  p.monitor_window_mm            = parser.seenval('W') ? parser.value_float() : 20.0f;
+  p.monitor_efficiency_pct       = parser.seenval('R') ? parser.value_float() : 85.0f;
+  p.monitor_confirm_windows      = parser.seenval('K') ? (uint8_t)parser.value_int() : 2;
+  p.pulse_gap_factor             = parser.seenval('G') ? parser.value_float() : 4.0f;
+  p.pulse_gap_min_ms             = parser.seenval('H') ? (uint16_t)parser.value_int() : 500;
   p.pulse_gap_min_missing_events = parser.seenval('X') ? parser.value_float() : 2.0f;
 
   makeit_fa_transaction.execute(point_id, p);
