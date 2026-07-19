0325fd13cd5199be89be9b132619d66e6d375584
gitpulssi <152341354+gitpulssi@users.noreply.github.com>
2026-07-18 17:53:08 -0700

    Define Phase-8 row limit for C++11 builds

---
+++ Marlin/src/feature/makeit_fa_envelope_constants.cpp
@@ -0,0 +1,12 @@
+/**
+ * C++11 out-of-class definition for the Phase-8 integral class constant.
+ */
+#include "../inc/MarlinConfig.h"
+
+#if ENABLED(MAKEIT_FILAMENT_ANALYZER_PHASE0)
+
+#include "makeit_fa_envelope.h"
+
+constexpr uint8_t MakeItFATemperatureEnvelope::MAX_ROWS;
+
+#endif // MAKEIT_FILAMENT_ANALYZER_PHASE0
